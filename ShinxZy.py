b9fc4ee5a106e97b5ae5726550393748a094a9e0
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2021-11-18 02:05:31 +0700

    Update ShinxZy.py

--- ShinxZy.py
+++ ShinxZy.py
@@ -102,7 +102,7 @@ logo = '''\033[0;92m programmer by:SHINZY \033[0m
  \033[0;93m [👉] Created by M.ZAINUL UMAM \033[0m
  \033[0;92m [👉] Email :zyshin41@gmail.com \033[0m
  \033[0;92m [👉] Bisnis:hig_rnh76@shinzy.com \033[0m
- \033[0;92m [👉] WA    :+6285740559154 \033[0m  [0;91mupdate V3.6 \033[0m '''
+ \033[0;92m [👉] WA    :+6285740559154 \033[0m  \033[0;96mupdate V3.6 \033[0m '''
 
 lo_ngentod = '1714009362122228'
 # crack selesai
