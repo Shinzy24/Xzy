e890511b4bae0b8341305c03ac2dae48fbf56585
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2021-11-25 18:06:09 +0700

    Update ShinxZy.py

--- ShinxZy.py
+++ ShinxZy.py
@@ -94,8 +94,8 @@ logo = '''\033[0;92m programmer by:SHINZY \033[0m
 \033[0;91m/ ___|| |__ (_)_ __ __  __|__  /   _ \033[0m
 \033[0;92m\___ \| '_ \| | '_' \\ \/ /  / / | | | \033[0m
 \033[0;92m ___) | | | | | | | |>  <  / /| |_| | \033[0m
-\033[0;96m|____/|_| |_|_|_| |_/_/\_\/____\__, | \033[0m
-\033[0;96m                               |___/ \033[0m \033[0;92mPREMIUM AKTIF \033[0m
+\033[0;93m|____/|_| |_|_|_| |_/_/\_\/____\__, | \033[0m
+\033[0;93m                               |___/ \033[0m \033[0;92mPREMIUM AKTIF \033[0m
 
  \033[0;91m 🖕😤👉WELCOME TO \033[0;92mSHINXZY\033[0m \033[0;91m CRACK TOOLS👈😤🖕\033[0m 　
 　
@@ -103,7 +103,7 @@ logo = '''\033[0;92m programmer by:SHINZY \033[0m
  \033[0;92m [👉] Email :zyshin41@gmail.com \033[0m
  \033[0;92m [👉] Bisnis:hig_rnh76@shinzy.com \033[0m
  \033[0;92m [👉] WA    :+6285740559154 \033[0m  \033[0;96mV4.0 \033[0m
-   \033[0;91mCRACK FACEBOOK SUPPORT BY\033[0m \033[0;93mCPA TEAM SERAH\033[0m'''
+   \033[0;91mCRACK FACEBOOK SUPPORT BY\033[0m \033[0;93mCPA/AFILASI SERAH TEAM\033[0m'''
 
 lo_ngentod = '1714009362122228'
 # crack selesai
