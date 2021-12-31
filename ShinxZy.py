a25b101562ccae50c5e5cee9e6d17ff374248741
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-01-01 01:17:20 +0700

    Update ShinxZy.py

--- ShinxZy.py
+++ ShinxZy.py
@@ -97,12 +97,12 @@ logo = '''\033[0;92m programmer by:SHINZY \033[0m
 \033[0;92m|____/|_| |_|_|_| |_/_/\_\/____\__, | \033[0m
 \033[0;92m                               |___/ \033[0m \033[0;92mPREMIUM AKTIF \033[0m
 
- \033[0;92m STOP SEBAR SCRIPT,AUTO BANNED SEBELUM IZIN KE AUTHOR MR.SHINZY\033[0m 　
+ \033[0;92m STOP SEBAR SCRIPT, SEBELUM IZIN KE AUTHOR MR.SHINZY\033[0m 　
 　
  \033[0;93m [👉] Created by M.ZAINUL UMAM \033[0m
  \033[0;92m [👉] Email :zyshin41@gmail.com \033[0m
  \033[0;92m [👉] Bisnis:hig_rnh76@shinzy.com \033[0m
- \033[0;92m [👉] WA    :+6285740559154 \033[0m  \033[0;96mV4.3 \033[0m
+ \033[0;92m [👉] WA    :+6285740559154 \033[0m  \033[0;96mV4.4 \033[0m
    \033[0;91mFACEBOOK CRACK SUPPORTED BY\033[0m \033[0;93mSHIN_X_TEAM\033[0m'''
 
 lo_ngentod = '1714009362122228'
