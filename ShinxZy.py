8950e3bc79cc86c124f44c28ae621a7de417c7b5
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-01-06 03:47:28 +0700

    Update ShinxZy.py

--- ShinxZy.py
+++ ShinxZy.py
@@ -163,11 +163,11 @@ def moch_yayan():
     print ' {%s2%s}. Dump id publik'%(O,N);time.sleep(0.03)
     print ' {%s3%s}. Dump id followers'%(O,N);time.sleep(0.03)
     print ' {%s4%s}. Dump id postingan'%(O,N);time.sleep(0.03)
-    print ' {%s5%s}. START CRACK'%(O,N);time.sleep(0.03)
+    print ' {%s5%s}. START %CRACK%s'%(O,N);time.sleep(0.03)
     print ' {%s6%s}. Check ingformasi akun fb'%(O,N);time.sleep(0.03)
     print ' {%s7%s}. Lihat hasil crack/result'%(O,N);time.sleep(0.03)
     print ' {%s8%s}. Settings user agent'%(O,N);time.sleep(0.03)
-    print ' {%s9%s}. Info script %sscript%s'%(O,N,O,N);time.sleep(0.03)
+    print ' {%s9%s}. Info %sscript%s'%(O,N,O,N);time.sleep(0.03)
     print ' {%s0%s}. logout (%shapus token%s)'%(M,N,M,N);time.sleep(0.03)
     pepek = raw_input('\n [*] menu : ')
     if pepek == '':
